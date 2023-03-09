6dc147de43386f93ab30ced8777405946f34df01
Luis <luisfernandosantaospina@gmail.com>
2023-03-09 10:59:59 -0500

    Add postgres connection

--- config.py
+++ config.py
@@ -3,12 +3,15 @@ from decouple import config
 
 class Config:
     SECRET_KEY = config('SECRET_KEY')
+    SQLALCHEMY_DATABASE_URI = 'postgresql://' +\
+        config('PGSQL_USER') + ':' +\
+        config('PGSQL_PASSWORD') + '@' + \
+        config('PGSQL_HOST') + ':' + \
+        config('PGSQL_PORT') + '/' + \
+        config('PGSQL_DATABASE')
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
+    PROPAGATE_EXCEPTIONS = True
 
 
 class DevelopmentConfig(Config):
     DEBUG = True
-
-
-config = {
-    'development': DevelopmentConfig
-}
