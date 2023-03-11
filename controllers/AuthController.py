a7622522d00013b55341eca9260e756ef8f35d9f
Luis <luisfernandosantaospina@gmail.com>
2023-03-11 13:51:11 -0500

    Add permissions to login

--- controllers/AuthController.py
+++ controllers/AuthController.py
@@ -1,8 +1,11 @@
 from flask_restful import Resource
-from models.models import User
+from models.models import User, UserSchema, Permission, PermissionSchema
 from flask import request
 from flask_jwt_extended import create_access_token
-from werkzeug.security import check_password_hash
+from werkzeug.security import check_password_hash, generate_password_hash
+
+user_schema = UserSchema()
+permission_schema = PermissionSchema()
 
 
 class AuthController(Resource):
@@ -12,9 +15,24 @@ class AuthController(Resource):
         email = request.json.get('email', None)
         password = request.json.get('password', None)
 
+        # password = 'luis'
+        # password_hash = generate_password_hash(password, method='sha256')
+        # print(password_hash)
+
         user = User.query.filter_by(email=email).first()
+
         if user is None or not check_password_hash(user.password, password):
             return {'mensaje': 'Credentials invalid'}, 401
 
         access_token = create_access_token(identity=user.id)
-        return {'access_token': access_token}, 200
+        permissions_by_role = user.role.permissions
+        permissions_user = []
+
+        for permission_by_role in permissions_by_role:
+            permissions_user.append(permission_by_role.name)
+
+        return {
+            'access_token': access_token,
+            'user': user_schema.dump(user),
+            'permissions': permissions_user
+        }, 200
